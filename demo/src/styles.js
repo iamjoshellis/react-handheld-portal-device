6895cacaa6c74581ef195c476d4806797603d06c
Josh Ellis <iamjoshellis@gmail.com>
2019-03-13 17:00:12 +1100

    Update demo styles 🎨

--- demo/src/styles.js
+++ demo/src/styles.js
@@ -1,6 +1,11 @@
 import styled, { css, createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
   html,
   body,
   #root {
